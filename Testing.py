01950e8bf471ec8f75db5ec4881e739a1c74a4a4
Aditya Solanki <56908240+adityasolanki205@users.noreply.github.com>
2021-01-13 23:37:17 +0530

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -40,22 +40,24 @@ def run(argv=None, save_main_session=True):
         csv_lines = (p 
                      | beam.io.ReadFromText(known_args.input,  skip_header_lines = 1) 
                      | beam.ParDo(Split()))
-        open_col = (csv_lines 
+        open_col  = (csv_lines 
                      | beam.ParDo(CollectOpen()) 
-                     | "Grouping Keys Open" >> beam.GroupByKey())
+                     | "Grouping Keys Open" >> beam.GroupByKey()
+                    )
         close_col = (csv_lines 
                      | beam.ParDo(CollectClose())
-                     | "Grouping Keys Close" >> beam.GroupByKey())
-        output = ( 
+                     | "Grouping Keys Close" >> beam.GroupByKey()
+                    )
+        '''output    = ( 
                     ({'Open'  : open_col, 
                       'Close' : close_col} 
                      | beam.CoGroupByKey())
                      | beam.io.WriteToText(known_args.output)
-                  )
-        '''output = ( (close_col, open_col)
+                    )'''
+        output = ( (close_col, open_col)
                      | beam.Flatten()
                      | beam.io.WriteToText(known_args.output)
-                  )'''
+                  )
         
 if __name__ == '__main__':
     run()
