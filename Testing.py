5e7927b184324575b185bfdef18c59d63e9e3274
Aditya Solanki <56908240+adityasolanki205@users.noreply.github.com>
2021-01-20 22:27:07 +0530

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -54,10 +54,14 @@ def run(argv=None, save_main_session=True):
                      | beam.CoGroupByKey())
                      | beam.io.WriteToText(known_args.output)
                     )'''
-        output = ( (close_col, open_col)
+        '''output = ( (close_col, open_col)
                      | beam.Flatten()
                      | beam.io.WriteToText(known_args.output)
-                  )
+                    )'''
+        output = (open_col 
+                      | 'Sum' >> beam.CombineValues(sum) 
+                      | beam.io.WriteToText(known_args.output)
+                     )
         
 if __name__ == '__main__':
     run()
